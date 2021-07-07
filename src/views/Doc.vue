5d6f9e886e812eb3b6248e92e2080f88a0ab306b
wly <1355498705@qq.com>
2021-07-07 16:30:13 +0800

    save

---
+++ src/views/Doc.vue
@@ -0,0 +1,67 @@
+<!--
+ * @file: 
+ * @Date: 2021-07-07 09:15:35
+ * @author: liuyong.wang
+-->
+<template>
+  HomePage
+  <router-link to="/demo1">demo1</router-link>
+  <router-link to="/demo2">demo2</router-link>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+
+import { ref } from 'vue'
+
+export default {
+  components: {  },
+  props: {
+    user: {
+      type: String,
+      required: true
+    }
+  },
+  setup (props: object) {
+    const repositories = ref([])
+    const getUserRepositories = async () => {
+      // repositories.value = await fetchUserRepositories(props.user)
+    }
+
+    return {
+      repositories,
+      getUserRepositories
+    }
+  },
+  data () {
+    return {
+      filters: { 
+
+      }
+    }
+  },
+  computed: {
+    filteredRepositories () { 
+
+     }, // 3
+    repositoriesMatchingSearchQuery () { 
+
+     }, // 2
+  },
+  watch: {
+    user: 'getUserRepositories' // 1
+  },
+  methods: {
+    updateFilters () { 
+
+     }, // 3
+  },
+  mounted () {
+    // this.getUserRepositories() // 1
+  }
+}
+</script>
+
+<style></style>
